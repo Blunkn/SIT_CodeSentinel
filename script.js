aa69a9a07d06e6cc2959d247cb7955831e516da8
trumenl <trumenlim@gmail.com>
2025-01-22 16:48:44 +0800

    Fixed js invoke issue
    The button triggers the script.js javascript now. Can work on the actual programming js from here now haha

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // script.js
-function myFunction() {
+
+// Wait for DOM to be ready before attaching event listeners
+document.addEventListener('DOMContentLoaded', () => {
+    const scanBtn = document.getElementById('scanBtn');
+    // Attach an event listener to the button
+    scanBtn.addEventListener('click', myFunction);
+  });
+  
+  // This function is called when the button is clicked
+  function myFunction() {
     alert('Button clicked! This is from an external JavaScript file.');
-}
+  }
+  
